ba6f29581ad7263c934f6e7fcadddc1fa9e53185
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-08-30 22:02:23 +0300

    modify connect file

--- connect.php
+++ connect.php
@@ -12,7 +12,6 @@ $option = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8" //FOR SUPPORTING ARABIC LANGUAGE
 );
 
-
 //connection code
 try {
     $con = new PDO($dsn, $user, $password, $option);
