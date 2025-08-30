6e13ca48be0fb465b7f55690734cef77dd49ccf8
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-08-30 22:30:31 +0300

    modify connect file

--- connect.php
+++ connect.php
@@ -1,25 +1,11 @@
 <?php
-
-$host = getenv("MYSQLHOST");
-$db   = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+$dsn = "mysql:host=" . getenv("MYSQLHOST") . ";dbname=" . getenv("MYSQLDATABASE") . ";port=" . getenv("MYSQLPORT");
 $user = getenv("MYSQLUSER");
-$password = getenv("MYSQLPASSWORD");
+$pass = getenv("MYSQLPASSWORD");
 
-$dsn = "mysql:host=$host;dbname=$db;port=$port;charset=utf8";
-
-$option = array(
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8" //FOR SUPPORTING ARABIC LANGUAGE
-);
-
-//connection code
 try {
-    $con = new PDO($dsn, $user, $password, $option);
+    $con = new PDO($dsn, $user, $pass);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    include __DIR__ . "/functions.php";
 } catch (PDOException $e) {
-    echo json_encode([
-        "status" => "failure",
-        "error" => $e->getMessage()
-    ]);
+    die("Connection failed: " . $e->getMessage());
 }
