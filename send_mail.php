9159b2ef1cf2e141cea3845f703876244a5e59d6
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-08-30 17:25:18 +0300

    replace smtp with sendgrid api

--- send_mail.php
+++ send_mail.php
@@ -1,41 +1,39 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 require __DIR__ . '/vendor/autoload.php';
 include_once(__DIR__ . "/functions.php");
 
+use SendGrid\Mail\Mail;
+
 function sendMail($to, $from, $subject, $title, $verificationCode)
 {
-    $mail = new PHPMailer(true);
+    $email = new Mail();
+    $email->setFrom($from, "Ecommerce App");
+    $email->setSubject($subject);
+    $email->addTo($to);
+    $email->addContent(
+        "text/html",
+        "
+            <h2>$title</h2>
+            <p>Your verification code is: <strong>$verificationCode</strong></p>
+        "
+    );
+    $email->addContent(
+        "text/plain",
+        "Your verification code is: $verificationCode"
+    );
+
+    $sendgrid = new \SendGrid(getenv("SENDGRID_API_KEY"));
 
     try {
-        //server settings
-        $mail->isSMTP();
-        $mail->Host = "smtp.sendgrid.net";
-        $mail->SMTPAuth = true;
-        $mail->Username = "apikey";
-        $mail->Password = getenv("SENDGRID_API_KEY");
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
+        $response = $sendgrid->send($email);
 
-
-        $mail->setFrom($from, "Ecommerce App");
-        $mail->addAddress($to);
-
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->Body    = "
-            <h2>$title</h2>
-            <p>Your verification code is: <strong>`$verificationCode`</strong></p>
-        ";
-        $mail->AltBody = "Your verification code is: `$verificationCode`";
-
-
-        $mail->send();
-        echo json_encode(array("status" => "Message has been sent successfully ✅"));
+        if ($response->statusCode() >= 200 && $response->statusCode() < 300) {
+            echo json_encode(array("status" => "success", "message" => "Message has been sent successfully ✅"));
+        } else {
+            printFailure("SendGrid error: " . $response->statusCode() . " - " . $response->body());
+        }
     } catch (Exception $e) {
-        printFailure("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        printFailure("Message could not be sent. Error: " . $e->getMessage());
     }
 }
