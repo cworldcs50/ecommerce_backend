9bc76b8f7eb8e055dde606026dbd983d388c56a2
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-08-31 14:30:52 +0300

    apply test on data

--- test.php
+++ test.php
@@ -1,8 +1,4 @@
 <?php
 include_once __DIR__ ."/send_mail.php";
 
-$isSend = sendMail("mohamedomer2582004@gmail.com", "mohamedomer2582004@gmail.com", "Verify your account", "Welcome to Ecommerce App", "123456");
-
-echo json_encode(
-    array("status" => $isSend)
-);
+getAllData("users");
