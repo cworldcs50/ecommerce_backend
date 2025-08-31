cc117a90eb2483e30a43b4ef54811c57e75195d8
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-08-31 11:45:24 +0300

    test connect file

--- connect.php
+++ connect.php
@@ -1,11 +1,21 @@
 <?php
-$dsn = "mysql:host=" . getenv("MYSQLHOST") . ";dbname=" . getenv("MYSQLDATABASE") . ";port=" . getenv("MYSQLPORT");
+$host = getenv("MYSQLHOST");
+$db   = getenv("MYSQLDATABASE");
 $user = getenv("MYSQLUSER");
 $pass = getenv("MYSQLPASSWORD");
+$port = getenv("MYSQLPORT");
+$option = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
+);
+
+echo $host . " " . $db . " " . $user . " " . $pass . " " . $port;
+
+$dsn = "mysql:host=$host;dbname=$db;port=$port";
 
 try {
-    $con = new PDO($dsn, $user, $pass);
+    $con = new PDO($dsn, $user, $pass, $option);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
