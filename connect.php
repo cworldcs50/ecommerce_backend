c2987948973fe8a438a33de57a0e8d36fb088cce
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-08-31 14:55:14 +0300

    solving error

--- connect.php
+++ connect.php
@@ -11,9 +11,10 @@ $option = array(
 $dsn = "mysql:host=$host;dbname=$db;port=$port";
 
 try {
-    include_once __DIR__ . "/functions.php";
     $con = new PDO($dsn, $user, $pass, $option);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
+
+include_once __DIR__ . "/functions.php";
