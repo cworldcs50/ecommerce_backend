80c5c7bfdc51dba9a7328d5415e8723b155c782d
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-08-31 11:54:06 +0300

    modify connect file

--- connect.php
+++ connect.php
@@ -1,21 +1,19 @@
 <?php
-$host = getenv("MYSQLHOST");
-$db   = getenv("MYSQLDATABASE");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$port = getenv("MYSQLPORT");
+$host = "yamanote.proxy.rlwy.net";
+$db   = "railway";
+$user = "root";
+$pass = "taNAXAfhzpinRjaROAzsrduWenWUaKmD";
+$port = 17225;
 $option = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
 );
-
-echo $host . " " . $db . " " . $user . " " . $pass . " " . $port;
 
 $dsn = "mysql:host=$host;dbname=$db;port=$port";
 
 try {
     $con = new PDO($dsn, $user, $pass, $option);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo json_encode(array("status" => "Connected successfully"));
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
