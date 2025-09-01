c50e5e401a718dc48c748aa3e47e2b0a16d3d22c
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-09-01 22:42:08 +0300

    modify off connect path in verification code file

--- auth/verfication_code.php
+++ auth/verfication_code.php
@@ -1,5 +1,5 @@
 <?php
-include_once  __DIR__ ."/connect.php";
+include_once  __DIR__ ."/../connect.php";
 
 $userEmail = filterRequest("userEmail");
 $userVerifyCode = filterRequest("userVerficationCode");
