fa0b9de106f85ead50e6fd3a5c2b9abd62bfdb25
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-09-02 14:08:33 +0300

    create reset password file

---
+++ forget_password/reset_password.php
@@ -0,0 +1,8 @@
+<?php
+include_once  __DIR__ . "/../connect.php";
+
+$userEmail = filterRequest("userEmail");
+$userNewPassword = sha1(filterRequest("userNewPassword"));
+$data = array("users_password" => $userNewPassword);
+
+updateData("users", $data, "users_email = '$userEmail'");
