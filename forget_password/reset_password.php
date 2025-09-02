4a5e3e7f70be53d90c849d97b500921c3536cfe7
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-09-02 18:40:47 +0300

    solve if the user enter the same password

--- forget_password/reset_password.php
+++ forget_password/reset_password.php
@@ -5,4 +5,21 @@ $userEmail = filterRequest("userEmail");
 $userNewPassword = sha1(filterRequest("userNewPassword"));
 $data = array("users_password" => $userNewPassword);
 
-updateData("users", $data, "`users_email` = '$userEmail'");
+$oldPassword = getOldPassword($userEmail);
+
+if ($oldPassword == $userNewPassword) {
+    echo json_encode(array("status" => "same password"));
+} else {
+    updateData("users", $data, "`users_email` = '$userEmail'");
+}
+
+function getOldPassword($userEmail)
+{
+    global $con;
+
+    $stmt = $con->prepare("SELECT `users_password` FROM `users` WHERE `users_email` = '$userEmail'");
+    $stmt->execute();
+    $oldPassword = $stmt->fetchColumn();
+
+    return $oldPassword;
+}
