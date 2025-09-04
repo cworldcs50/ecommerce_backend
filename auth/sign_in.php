4ebb2e8b8255163c47bc1250c51dd3b475a506fb
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-09-04 16:27:59 +0300

    modify sign_in file with getData method

--- auth/sign_in.php
+++ auth/sign_in.php
@@ -4,13 +4,4 @@ include_once  __DIR__ . "/../connect.php";
 
 $userPassword = sha1(filterRequest("userPassword"));
 $userEmail = filterRequest("userEmail");
-
-$stmt = $con->prepare("SELECT * FROM `users` WHERE `users_password` = ? AND `users_email` = ? AND `users_approve` = '1'");
-$stmt->execute(array($userPassword, $userEmail));
-$count = $stmt->rowCount();
-
-if ($count > 0) {
-    echo json_encode(array("status" => "success"));
-} else {
-    printFailure("email or password are not correct");
-}
+getData("users", "`users_password` = ? AND `users_email` = ? AND `users_approve` = '1'", array($userPassword, $userEmail));
