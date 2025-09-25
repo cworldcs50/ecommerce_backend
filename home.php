c8006cfd917997281e90fd13b4744f423dce4166
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-09-25 17:15:34 +0300

    add get all items to home page

--- home.php
+++ home.php
@@ -6,6 +6,8 @@ include_once(__DIR__ . "/connect.php");
 $allData = array();
 
 $allData["categories"] = getAllData("categories", null, null, false);
+$allData["categories"] = getAllData("items", "items_discount != 0", null, false);
 $allData["status"] = "success";
+
 
 echo json_encode($allData);
