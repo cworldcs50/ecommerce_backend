03357cef15bcc84782006a62af50855507c8d0c1
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-09-25 17:57:55 +0300

    use view instead of items table

--- home.php
+++ home.php
@@ -6,7 +6,7 @@ include_once(__DIR__ . "/connect.php");
 $allData = array();
 
 $allData["categories"] = getAllData("categories", null, null, false);
-$allData["items"] = getAllData("items", "items_discount != 0", null, false);
+$allData["items"] = getAllData("items_view", "items_discount != 0", null, false);
 $allData["status"] = "success";
 
 
