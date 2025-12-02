713efa3e8699e5ec7c8201a0eaf03d8504897771
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-12-02 22:39:06 +0200

    more clean code of items

--- items/items.php
+++ items/items.php
@@ -5,14 +5,24 @@ $userId = filterRequest("user_id");
 $categoryId = filterRequest("category_id");
 
 $query = "SELECT items_view.*, 1 AS is_favorite
-FROM items_view INNER JOIN favorites ON favorites.favorites_items_id = items_view.items_id
-INNER JOIN users ON favorites.favorites_users_id = ? 
-WHERE items_view.categories_id = ?
+    FROM items_view 
+    INNER JOIN favorites 
+    ON favorites.favorites_items_id = items_view.items_id
+    INNER JOIN users 
+    ON favorites.favorites_users_id = ? 
+    WHERE items_view.categories_id = ?
+
 UNION ALL
-SELECT items_view.*, 0 AS is_favorite FROM items_view
-WHERE items_view.categories_id = ? AND items_view.items_id NOT IN ( SELECT items_view.items_id FROM items_view INNER JOIN favorites
-ON favorites.favorites_items_id = items_view.items_id
-INNER JOIN users ON favorites.favorites_users_id = ?);";
+
+SELECT items_view.*, 0 AS is_favorite 
+FROM items_view
+    WHERE items_view.categories_id = ? AND items_view.items_id 
+    NOT IN ( SELECT items_view.items_id 
+    FROM items_view 
+    INNER JOIN favorites
+    ON favorites.favorites_items_id = items_view.items_id
+    INNER JOIN users 
+    ON favorites.favorites_users_id = ?);";
 
 $stmt = $con->prepare($query);
 $stmt->execute(array($userId, $categoryId, $categoryId, $userId));
