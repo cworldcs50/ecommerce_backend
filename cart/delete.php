bd6611134448345435a741184caed3efd98fbc4d
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2026-01-03 20:04:46 +0200

    modify small error

--- cart/delete.php
+++ cart/delete.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 include_once  __DIR__ . "/../connect.php";
 
 $userId = filterRequest("userId");
 $itemId = filterRequest("itemId");
 
-deleteData("cart", "cart_id = (select * from cart where user_id = $userId and cart_item_id = $itemId LIMIT 1)");
+deleteData("cart", "cart_id = (select cart_id from cart where user_id = $userId and cart_item_id = $itemId LIMIT 1)");
