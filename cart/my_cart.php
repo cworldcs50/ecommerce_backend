75b4ca9a122df7d00b95a8e67f046df60ac1f346
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2026-01-11 19:52:24 +0200

    my cart view

--- cart/my_cart.php
+++ cart/my_cart.php
@@ -3,4 +3,21 @@ include_once  __DIR__ . "/../connect.php";
 
 $userId = filterRequest("userId");
 
-getAllData("cart_view", "user_id = ?", array($userId));
+$data = getAllData("cart_view", "user_id = ?", array($userId), false);
+
+$stmt = $con->prepare("SELECT SUM(cart_view.item_total_price) as total_price, COUNT(cart_view.no_item) as total_count 
+                       FROM cart_view 
+                       WHERE cart_view.user_id = ?
+                       GROUP BY cart_view.user_id;"
+);
+
+$stmt->execute(array($userId));
+
+$dataCountAndPrice = $stmt->fetch(PDO::FETCH_ASSOC);
+
+echo json_encode(
+    array(
+        "cartData" => $data, 
+        "CountAndPriceData" => $dataCountAndPrice
+    )
+);
